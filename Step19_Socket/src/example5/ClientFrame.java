7840dc2a80724abb6725822cfe599235b9648ecd
홍주 <@DESKTOP-9F9NLNN>
2020-12-14 14:22:08 +0900

    Step19_example5 예제 추가

---
+++ Step19_Socket/src/example5/ClientFrame.java
@@ -0,0 +1,129 @@
+package example5;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class ClientFrame extends JFrame implements ActionListener{
+	//필드
+	JTextField tf;
+	Socket socket;
+	BufferedWriter bw;
+	BufferedReader br;
+	JTextArea ta;
+	
+	//생성자
+	public ClientFrame(String title) {
+		super(title);
+		setLayout(new BorderLayout());
+		
+		//페널을 만들어서 프레임의 위쪽에 배치하기 
+		JPanel topPanel=new JPanel();
+		topPanel.setBackground(Color.PINK);
+		add(topPanel, BorderLayout.NORTH);
+		
+		//프레임에 UI 배치하기
+		tf=new JTextField(10);
+		JButton sendBtn=new JButton("Send");
+		topPanel.add(tf);
+		topPanel.add(sendBtn);
+		//버튼 리스너 등록하기
+		sendBtn.addActionListener(this);
+		//채팅 메세지를 출력할 TextArea 객체를 생성해서
+		ta=new JTextArea();
+		//스크롤 가능한 UI에 포장 후
+		JScrollPane scPane=new JScrollPane(ta, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		//스크롤 가능한 UI를 프레임의 가운데에 배치한다.
+		add(scPane, BorderLayout.CENTER);
+		//오직 출력 용도로 사용하기 위해
+		ta.setEditable(false);
+		
+		//소켓 접속하기 (생성자가 길어져서 메소드로 따로 뺌)
+		connect();
+	}
+	
+	public static void main(String[] args) {
+		//프레임객체 생성하면서 프레임의 제목을 chatting 으로 지정하기 
+		ClientFrame f=new ClientFrame("chatting");
+		//프레임의 x좌표, y좌표, width, height 설정하기
+		f.setBounds(500, 100, 500, 500);
+		//프레임을 닫을때 프로세스도 종료 되도록 한다. 
+		f.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		//프레임을 화면상에 보이게 하기 
+		f.setVisible(true);
+		//소켓 접속하기
+	}
+
+	//Socket 서버에 접속을 하는 메소드
+	public void connect() {
+		try {
+			//소켓 객체
+		socket=new Socket("14.63.164.99", 5000);
+		//서버에 문자열을 출력할 객체
+		bw=new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+		//서버가 전송하는 문자열을 읽어들일 객체
+		br=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		//새로운 스레드를 시작 시켜서 서버에서 문자열이 도착해는지 지속적으로 대기한다.
+		new ClientThread().start();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		//1. JTextField 에 입력한 문자열을 읽어와서
+		String msg=tf.getText();
+		//2. BufferedWriter 객체를 이용해서 출력한다.
+		try {
+			bw.write(msg);
+			bw.newLine();
+			bw.flush();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		//3. JTextField 에 입력한 문자열 삭제
+		tf.setText("");
+	}
+	
+	//서버에서  문자열이 전송되는지 지속적으로 대기하는 스레드 객체를 생성할 클래스 설계
+	class ClientThread extends Thread{
+		@Override
+		public void run() {
+			while(true) {
+				try {
+					//대기하다가 문자열이 도착하면 메소드가 리턴한다.
+					String line=br.readLine();
+					//도착된 메세지를 JTextArea에 추가하기
+					ta.append(line+"\r\n");
+					//출력할 문서의 높이
+					int height=ta.getDocument().getLength();
+					//높이만큼 JTextArea 를 스크롤시켜서 가장 아래에 있는 문자열이 보이게
+					ta.setCaretPosition(height);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
+
+
+
+
---
+++ Step19_Socket/src/example5/ServerMain.java
@@ -0,0 +1,125 @@
+package example5;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServerMain {
+	//접속한 클라이언트를 응대하는 스레드 객체의 '목록'을 담을 필드 목록-스레드 객체가 여러개 생긴다는 말.
+	static List<ServerThread> threadList; //static 을 사용하는 이유? 메인메소드에서만 사용하려면 static이 있어야 한다. 
+	
+	public static void main(String[] args) {//run 하게 되면 들어오는 메인 스레드(작업단위) static main 이기 때문에 
+		/* 
+		 * static main 에서 참조하기 위해서는 필드, 클래스 전부 static이 붙어있어야 한다. 
+		 * static은 구분된 공간이라는 것 기억하기.
+		 */
+		threadList=new ArrayList<ServerMain.ServerThread>();
+		ServerSocket serverSocket=null;
+		
+		try {
+			//5000 번 통신 port 를 열고 클라이언트의 접속을 기다린다.
+			serverSocket=new ServerSocket(5000);
+			/*
+			 *  .accept() 메소드는 클라이언트가 접속을 해야지 리턴하는 메소드이다.
+			 *  클라이언트가 접속을 해오면 해당 클라이언트와 연결된 Socket 객체의
+			 *  참조값을 리턴한다. 
+			 */
+			while(true) {//메인스레드가 하는 일. 접속을 기다리다가 접속을 하면 스레드 시작. 반복.
+				System.out.println("클라이언트의 Socket 접속을 기다립니다...");
+				//1.클라이언트가 접속을 해오면
+				Socket socket=serverSocket.accept();
+				//2.새로운 스레드를 시작 시킨다.
+				ServerThread t=new ServerThread(socket);
+				t.start();
+				//하는 일은 ServerThread에 코딩을 하겠다. 
+				
+				//3.시작된 스레드의 참조값을 목록에 누적 시킨다.
+				threadList.add(t);//소캣객체의 참조값이 다 다른 스레드가 누적된다. 10명이 들어오면 10개의 참조값이 다 다르다.
+				/* 
+				 * 스레드들은 전부 독립적으로 움직인다.
+				 * 각각 접속한 클라이언트의 숫자만큼 생겨서 일을 진행.(다들 하는 일이 다르기 때문)
+				 * 
+				 */
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("ServerMain main 메소드가 종료 됩니다.");
+	}
+	
+	//스레드 클래스 설계 (main 클래스가 시작되었을 때 스레드가 하는 일 코딩) static이 붙은 이유. 메인 메소드에 사용하려면 내부 클래스에서도 static을 붙여야 한다. 아니면 참조할 수 없음. 
+	static class ServerThread extends Thread{//스레드에서 상속받기
+		
+		//필드(클라이언트와 연결된 Socket 객체의 참조값을 저장할 필드)
+		private Socket socket;
+		//클라이언트에게 문자열을 출력할 객체의 참조값을 저장할 필드
+		private BufferedWriter bw;
+		
+		//생성자
+		public ServerThread(Socket socket) {
+			//생성자의 인자로 전달받은 Socket 객체의 참조값을 필드에 저장하기
+			this.socket=socket;
+			
+		}
+		
+		//인자로 전달되는 문자열을 Socket 객체를 통해서 출력하는 메소드
+		public void broadcast(String msg) throws IOException { //서버스레드 메소드는 브로드캐스트라는 메소드를 갖게 되었다. 
+			//인자로 전달된 문자열을 필드에 저장된 bufferedWriter 객체의 참조값을 이용해서 클라이언트에게 출력하기
+			bw.write(msg);
+			bw.newLine();//개행기호
+			bw.flush();//방출
+			/*
+			 * bw는 이미 트라이캐치로 묶인 곳에서 호출하는 것이기 때문에
+			 * throws해도 밑에서 묶여있으므로 상관없다. 
+			 * *나의 질문*
+			 * bw가 이미 트라이캐치 안에 묶여있는 상태에서 호출을 하는 것이기 때문에 throws로 넘겨버려도 상관없다는 건가요?
+			 * close는 모든 것이 끝나고 나서 닫는다. 
+			 */
+		}
+		
+		@Override
+		public void run() {
+			try {
+				
+//				InputStream is=socket.getInputStream();
+//				InputStreamReader isr=new InputStreamReader(is);
+//				BufferedReader br=new BufferedReader(isr);
+				
+				//위의 코드 한줄로 바꾸기
+				//클라이언트가 전송하는 문자열을 읽어들일 객체
+				BufferedReader br=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				
+				//클라이언트에게 문자열을 출력할 객체
+				bw=new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+				
+				//반복문 돌면서 클라이언트가 전송하는 문자열이 있는지 읽어와 본다.
+				while(true) {
+					//문자열 한줄이 전송될 때 까지 블록킹 되는 메소드
+					String line=br.readLine();//접속한 클라이언트의 갯수만큼 생성될 예정.(갯수만큼 있다.) 클라이언트가 전송한 문자열을 읽어와서
+					//반복문 돌면서 모든 클라이언트에게 전송하기. 어떤 클라이언트가 문자를 보내면 모든 클라이언트에게 중계를 하겠다.
+					for(ServerThread tmp:threadList) { // *확장 for문 확실히 알기 분석!*
+						tmp.broadcast(line);
+					}
+					
+				}
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
+/* 
+ * heap 영역에 스레드가 여러개 생김. 클라이언트가 들어온 만큼...
+ */
+
+
+
+
